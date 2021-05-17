ac02b4b941db07c6567f09732ef80d2d34cb3d90
Williams25 <william.souza.timao@hotmail.com>
2021-05-17 18:10:17 -0300

    foi criado page home que possue os cards das tecnologias e um modal de login e para se cadastrar-se

---
+++ pages/index.tsx
@@ -0,0 +1,38 @@
+import { useContext } from "react";
+import {
+  experimentalStyled as styled,
+  Container,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import { KeepMountedModal, ListCard } from "../src/components";
+import { UserContext } from "../src/contexts";
+import data from "../data.json";
+
+export default function Home() {
+  const { userNameContext } = useContext(UserContext);
+  return (
+    <>
+      <Container>
+        <Grid
+          sx={{
+            width: "100%",
+            marginTop: "2rem",
+            marginBottom: "2rem",
+            flexGrow: 1,
+          }}
+          container
+          spacing={1}
+          justifyContent="flex-start"
+          alignItems="center"
+        >
+          <KeepMountedModal />
+          <Typography variant="h6" component="span">
+            Bem vindo {userNameContext}!
+          </Typography>
+        </Grid>
+        <ListCard data={data.technologies} />
+      </Container>
+    </>
+  );
+}
